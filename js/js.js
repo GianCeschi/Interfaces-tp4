795c4e70d2f569d4054b799efe1a7dd82dbd6c0c
Gianluca Ceschinelli <ceschinelligianluca@gmail.com>
2023-11-19 13:52:09 -0300

    modificamos header sticky y se realiza cruz de menu hamburguesa

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 // Obtén el contenedor de la imagen y la posición donde debería aparecer
 let logo = document.getElementById('logo');
-let triggerHeight = 300; // Ajusto este valor para que aparezca el logo chiquito cuando yo quiero
+let triggerHeight = 150; // Ajusto este valor para que aparezca el logo chiquito cuando yo quiero
 
 // Agrega un evento de scroll
 window.addEventListener('scroll', function() {
@@ -10,9 +10,9 @@ window.addEventListener('scroll', function() {
   // Verifica si la posición de desplazamiento alcanza el punto de activación
   if (scrollPosition > triggerHeight) {
     // Agrega la clase 'visible' para hacer visible la imagen
-    logo.classList.add('visible');
+    logo.classList.add('pequeño');
   } else {
     // Remueve la clase 'visible' si la posición de desplazamiento está por debajo del punto de activación
-    logo.classList.remove('visible');
+    logo.classList.remove('pequeño');
   }
 });
